23f147c4b4774f19088ae1c2a04ec96e47bc4d6e
Xein <xgonip@gmail.com>
2025-12-21 19:59:20 +0100

    Fix spine-runtimes package to build without CMake

--- packages/s/spine-runtimes/xmake.lua
+++ packages/s/spine-runtimes/xmake.lua
@@ -5,18 +5,7 @@ set_license("Spine Runtimes")
 
 add_urls("https://github.com/EsotericSoftware/spine-runtimes.git")
 
-add_versions("3.8", "d33c10f85634d01efbe4a3ab31dabaeaca41230c")
-add_versions("4.0", "4.0")
-add_versions("4.1", "4.1")
 add_versions("4.2", "4.2")
-
-add_patches("3.8", "patches/3.8/cmake.patch")
-add_patches("4.0", "patches/4.x/spine-cpp-only.patch", "a6343bedac1a72657d65765d35321113160fe6622b28c79242e60e5a71d353be")
-add_patches("4.1", "patches/4.x/spine-cpp-only.patch", "a6343bedac1a72657d65765d35321113160fe6622b28c79242e60e5a71d353be")
-add_patches("4.2", "patches/4.x/spine-cpp-only.patch", "a6343bedac1a72657d65765d35321113160fe6622b28c79242e60e5a71d353be")
-add_patches("4.0", "patches/4.x/default_extension_shim.patch", "903a49c9d873956b517374daf232cb25509ee0df0ed3aa6cd44cfa337da3492a")
-add_patches("4.1", "patches/4.x/default_extension_shim.patch", "903a49c9d873956b517374daf232cb25509ee0df0ed3aa6cd44cfa337da3492a")
-add_patches("4.2", "patches/4.x/default_extension_shim.patch", "903a49c9d873956b517374daf232cb25509ee0df0ed3aa6cd44cfa337da3492a")
 
 add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
@@ -24,33 +13,39 @@ if is_host("windows") then
 	set_policy("platform.longpaths", true)
 end
 
-add_deps("cmake")
-
 on_install(function (package)
-	local configs = {
-		"-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
-		"-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF")
-	}
-	import("package.tools.cmake").install(package, configs)
+	local configs = {}
+	if package:is_debug() then
+		table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
+	else
+		table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
+	end
+	
+	-- Navigate to spine-cpp directory
+	os.cd("spine-cpp")
+	
+	-- Copy source files
+	io.writefile("xmake.lua", [[
+add_rules("mode.debug", "mode.release")
+
+target("spine-cpp")
+	set_kind("static")
+	add_files("spine-cpp/src/spine/**.cpp")
+	add_headerfiles("spine-cpp/include/(spine/**.h)")
+	add_includedirs("spine-cpp/include", {public = true})
+	set_languages("c++11")
+]])
+	
+	import("package.tools.xmake").install(package)
 end)
 
 on_test(function (package)
-	if package:version():ge("4.0") then
-		assert(package:check_cxxsnippets({test = [[
+	assert(package:check_cxxsnippets({test = [[
 		#include <spine/SkeletonData.h>
 		#include <spine/Skeleton.h>
 		void test() {
 			spine::SkeletonData data;
 			(void)data.getName();
 		}
-		]]}, {configs = {languages = "c++17"}}))
-	else
-		assert(package:check_cxxsnippets({test = [[
-		#include <spine/spine.h>
-		void test() {
-			spine::Atlas atlas(0, 0, 0, 0);
-			(void)atlas.getPages();
-		}
-		]]}, {configs = {languages = "c++14"}}))
-	end
+	]]}, {configs = {languages = "c++11"}}))
 end)
